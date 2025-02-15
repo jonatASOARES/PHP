bfe4f125eafd3600565c3fb3e9c92fdb9fe08e58
Jonatas. S. Silva <jonatasoares_dasilva@hotmail.com>
2025-02-15 16:32:44 -0300

    Foi alterada a treceira linha

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo "Hello, World!";
-echo "Esta essa é uma linha nova 13/02/2025"
+echo "Esta essa é uma linha nova 13/02/2025 pelo github"
+echo "Mais uma linha adcionada no dia 15/02/2025 pelo computador"
 ?>
