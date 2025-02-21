5036e3b13c33b9b4e57e2237e11600b49990145e
jonatASOARES <66322747+jonatASOARES@users.noreply.github.com>
2025-02-21 19:51:45 -0300

    21/02/2025 index.php
    linha adiciona 21/02/2025

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo "Hello, World!";
 echo "Esta essa é uma linha nova 13/02/2025"
 echo "Nova linha acionada partido do meu computador 21/02/2025"
+echo "linha adicionada pelo github 21/02/2025"
 ?>
