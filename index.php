3f4931027bd37ca25b9d338dce5b31278a9dbdba
Jonatas. S. Silva <jonatasoares_dasilva@hotmail.com>
2025-02-23 16:22:45 -0300

    texto criado da primeira branch

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ echo "Nova linha acionada partido do meu computador 21/02/2025"
 echo "linha adicionada pelo github 21/02/2025"
 echo "Programa pronto para teste , alterado pelo meu computador. Beta.1"
 echo "novo paragrafo editado"
+echo "primeiro texto da branch editado 23/02/2025 as 16:20 "
 ?>
