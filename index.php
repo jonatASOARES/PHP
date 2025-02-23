cc6e6bc1ccbe2d32e6418458fc2581cc3b64afc8
Jonatas. S. Silva <jonatasoares_dasilva@hotmail.com>
2025-02-22 22:27:52 -0300

    projeto beta.1 22/02/2025

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ echo "Hello, World!";
 echo "Esta essa é uma linha nova 13/02/2025"
 echo "Nova linha acionada partido do meu computador 21/02/2025"
 echo "linha adicionada pelo github 21/02/2025"
+echo "Programa pronto para teste , alterado pelo meu computador. Beta.1"
 ?>
