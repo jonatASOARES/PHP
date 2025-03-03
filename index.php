506e48b749e2caba438717a563898e2d59e06ade
Jonatas. S. Silva <jonatasoares_dasilva@hotmail.com>
2025-03-03 16:46:44 -0300

    cor alterada para vermelha

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-echo "Hello, World!";
-echo "Esta essa é uma linha nova 13/02/2025"
-echo "Nova linha acionada partido do meu computador 21/02/2025"
-echo "linha adicionada pelo github 21/02/2025"
-echo "Programa pronto para teste , alterado pelo meu computador. Beta.1"
-echo "novo paragrafo editado"
-echo "primeiro texto da branch editado 23/02/2025 as 16:20 "
+encho"cor vermelha css3" 
 ?>
